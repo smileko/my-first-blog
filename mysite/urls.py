e199ea122ed1dd9ec4a0893c2506fd44b69973e6
KoJungSu <kjjsss2@hanmail.net>
2020-05-08 16:47:45 +0900

    Added preview, previwe list, remove, edit, approval, login and comment function

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls import url, include
+
+from django.contrib.auth import views
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^accounts/login/$', views.login, name='login'),
+    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page':'/'}),
+    # path('login/', views.login, name='login'),
     path('', include('blog.urls')),
 ]
