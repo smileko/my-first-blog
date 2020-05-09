76a516f7bd122e83889ff89d3c7e0256100ff1dd
KoJungSu <kjjsss2@hanmail.net>
2020-05-09 15:36:02 +0900

    Changed from login to LoginView

--- mysite/urls.py
+++ mysite/urls.py
@@ -24,8 +24,8 @@ from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^accounts/login/$', views.login, name='login'),
-    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page':'/'}),
+    url(r'^accounts/login/$', views.LoginView, name='login'),
+    url(r'^accounts/logout/$', views.LogoutView, name='logout', kwargs={'next_page':'/'}),
     # path('login/', views.login, name='login'),
     path('', include('blog.urls')),
 ]
